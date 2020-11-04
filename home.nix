2d3432d9611c706be16e109ee7f99b5f5339718d
Suwon Park <sepiabrown@naver.com>
2020-11-04 12:37:34 +0900

    201104

--- home.nix
+++ home.nix
@@ -105,6 +105,8 @@ in
     #   signByDefault = true;
     # };
   };
+
+  programs.gh.enable = true;
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
